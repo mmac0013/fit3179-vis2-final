6dd04bd72bdb066fa8a9a3a4bb0f9b0927b92252
mmac0013 <91102635+mmac0013@users.noreply.github.com>
2021-09-26 12:06:04 +1000

    Delete vega_lite_vis.js

--- vega_lite_vis.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "js/choropleth_map.vg.json";
-vegaEmbed("#choropleth_chart", vg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
